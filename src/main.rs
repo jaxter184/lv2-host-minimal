4a2c29cf897d21242d65b2af76a1ab57613d4c70
Cody Bloemhard <cblmhrd@live.nl>
2021-04-28 20:57:01 +0200

    prev commit about create_ports being a done was a joke :)

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
         let plugin = lilv_plugins_get_by_uri(plugins, uri);
         lilv_node_free(uri);
         println!("{:?}", plugin); // bruh moment: if i dont print this plugin evaluates to 0x0 but if i do it's a value and it's fine.
-        let ports = create_ports(world, plugin);
+        let (ports, n_audio_in, n_audio_out) = create_ports(world, plugin);
     }
     println!("Hello, world!");
 }
@@ -32,20 +32,28 @@ struct Port{
     optional: bool,
 }
 
-unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> Vec<Port>{
+// ([ports], n_audio_in, n_audio_out)
+unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> (Vec<Port>, usize, usize){
     println!("{:?}", plugin); // this checks if our weird ass pointer becomes 0x0 or not
     if world == std::ptr::null_mut() { panic!("create_ports: world is null"); }
     if plugin == std::ptr::null_mut() { panic!("create_ports: plugin is null"); }
+
     let mut ports = Vec::new();
+    let mut n_audio_in = 0;
+    let mut n_audio_out = 0;
+
     let n_ports = lilv_plugin_get_num_ports(plugin);
+    println!("Ports found: {}", n_ports);
+
     let values = vec![0.0f32; n_ports as usize];
     lilv_plugin_get_port_ranges_float(plugin, std::ptr::null_mut(), std::ptr::null_mut(), values.as_ptr() as *mut f32);
+
     let lv2_input_port = lilv_new_uri(world, LILV_URI_INPUT_PORT.as_ptr() as *const i8);
     let lv2_output_port = lilv_new_uri(world, LILV_URI_OUTPUT_PORT.as_ptr() as *const i8);
     let lv2_audio_port = lilv_new_uri(world, LILV_URI_AUDIO_PORT.as_ptr() as *const i8);
     let lv2_control_port = lilv_new_uri(world, LILV_URI_CONTROL_PORT.as_ptr() as *const i8);
     let lv2_connection_optional = lilv_new_uri(world, LILV_URI_CONNECTION_OPTIONAL.as_ptr() as *const i8);
-    println!("Ports found: {}", n_ports);
+
     for i in 0..n_ports{
         let lport = lilv_plugin_get_port_by_index(plugin, i);
         let value = if values[i as usize].is_nan() { 0.0 } else { values[i as usize] };
@@ -56,7 +64,14 @@ unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> 
         else { false };
 
         let ptype = if lilv_port_is_a(plugin, lport, lv2_control_port) { PortType::Control }
-        else if lilv_port_is_a(plugin, lport, lv2_audio_port) { PortType::Audio }
+        else if lilv_port_is_a(plugin, lport, lv2_audio_port) {
+            if is_input{
+                n_audio_in += 1;
+            } else {
+                n_audio_out += 1;
+            }
+            PortType::Audio
+        }
         else if !optional { panic!("oof"); }
         else { PortType::Other };
 
@@ -76,5 +91,5 @@ unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> 
     lilv_node_free(lv2_control_port);
     lilv_node_free(lv2_connection_optional);
 
-    ports
+    (ports, n_audio_in, n_audio_out)
 }
