02aa56f6d001e8f890084f20960101e0274604da
Cody Bloemhard <cblmhrd@live.nl>
2021-04-28 20:49:10 +0200

    fix is_input, ptype, LILV_URI_CONNECTION_OPTIONAL, create_ports done

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use lilv_sys::*;
 // https://github.com/wmedrano/Olivia/tree/main/lilv
 
 // missing binding?
-pub const LILV_URI_CONNECTION_OPTIONAL: &'static [u8; 49usize] = b"https://lv2plug.in/ns/lv2core#connectionOptional\0";
+pub const LILV_URI_CONNECTION_OPTIONAL: &[u8; 48usize] = b"http://lv2plug.in/ns/lv2core#connectionOptional\0";
 
 fn main() {
     unsafe{
@@ -16,15 +16,15 @@ fn main() {
         let plugin = lilv_plugins_get_by_uri(plugins, uri);
         lilv_node_free(uri);
         println!("{:?}", plugin); // bruh moment: if i dont print this plugin evaluates to 0x0 but if i do it's a value and it's fine.
-        create_ports(world, plugin);
+        let ports = create_ports(world, plugin);
     }
     println!("Hello, world!");
 }
 
-enum PortType{ CONTROL, AUDIO }
+enum PortType{ Control, Audio, Other }
 
 struct Port{
-    lilv_port: LilvPort,
+    lilv_port: *const LilvPort,
     ptype: PortType,
     index: u32,
     value: f32,
@@ -32,8 +32,11 @@ struct Port{
     optional: bool,
 }
 
-unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl){
+unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> Vec<Port>{
     println!("{:?}", plugin); // this checks if our weird ass pointer becomes 0x0 or not
+    if world == std::ptr::null_mut() { panic!("create_ports: world is null"); }
+    if plugin == std::ptr::null_mut() { panic!("create_ports: plugin is null"); }
+    let mut ports = Vec::new();
     let n_ports = lilv_plugin_get_num_ports(plugin);
     let values = vec![0.0f32; n_ports as usize];
     lilv_plugin_get_port_ranges_float(plugin, std::ptr::null_mut(), std::ptr::null_mut(), values.as_ptr() as *mut f32);
@@ -42,19 +45,36 @@ unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl){
     let lv2_audio_port = lilv_new_uri(world, LILV_URI_AUDIO_PORT.as_ptr() as *const i8);
     let lv2_control_port = lilv_new_uri(world, LILV_URI_CONTROL_PORT.as_ptr() as *const i8);
     let lv2_connection_optional = lilv_new_uri(world, LILV_URI_CONNECTION_OPTIONAL.as_ptr() as *const i8);
-    println!("{}", n_ports);
+    println!("Ports found: {}", n_ports);
     for i in 0..n_ports{
-        println!("{}", i);
         let lport = lilv_plugin_get_port_by_index(plugin, i);
         let value = if values[i as usize].is_nan() { 0.0 } else { values[i as usize] };
         let optional = lilv_port_has_property(plugin, lport, lv2_connection_optional);
+
         let is_input = if lilv_port_is_a(plugin, lport, lv2_input_port) { true }
-        else if lilv_port_is_a(plugin, lport, lv2_output_port) && !optional { println!("Port is neither input nor output."); false }
+        else if !lilv_port_is_a(plugin, lport, lv2_output_port) && !optional { panic!("Port is neither input nor output."); }
         else { false };
 
-        let ptype = if lilv_port_is_a(plugin, lport, lv2_control_port) { PortType::CONTROL }
-        else if lilv_port_is_a(plugin, lport, lv2_audio_port) { PortType::AUDIO }
-        else if !optional { panic!("oof"); PortType::CONTROL }
-        else { PortType::CONTROL };
+        let ptype = if lilv_port_is_a(plugin, lport, lv2_control_port) { PortType::Control }
+        else if lilv_port_is_a(plugin, lport, lv2_audio_port) { PortType::Audio }
+        else if !optional { panic!("oof"); }
+        else { PortType::Other };
+
+        ports.push(Port{
+            lilv_port: lport,
+            ptype,
+            index: i,
+            value ,
+            is_input,
+            optional,
+        });
     }
+
+    lilv_node_free(lv2_input_port);
+    lilv_node_free(lv2_output_port);
+    lilv_node_free(lv2_audio_port);
+    lilv_node_free(lv2_control_port);
+    lilv_node_free(lv2_connection_optional);
+
+    ports
 }
