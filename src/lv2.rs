556ed04432499672fac3af60546ceace86017a8f
Cody Bloemhard <cblmhrd@live.nl>
2021-05-01 23:13:51 +0200

    make port values setable

--- src/lv2.rs
+++ src/lv2.rs
@@ -2,6 +2,7 @@ use lilv_sys::*;
 use std::ffi::{ CStr };
 use std::ptr;
 use std::ffi;
+use std::collections::HashMap;
 // find plugins in /usr/lib/lv2
 // URI list with lv2ls
 // https://docs.rs/lilv-sys/0.2.1/lilv_sys/index.html
@@ -16,6 +17,7 @@ pub struct Lv2Host{
     lilv_plugins: *const LilvPlugins,
     plugins: Vec<Plugin>,
     uri_home: Vec<String>,
+    plugin_names: HashMap<String, usize>,
     in_buf: [f32; 2],
     out_buf: [f32; 2],
 }
@@ -30,12 +32,13 @@ impl Lv2Host{
             lilv_plugins,
             plugins: Vec::new(),
             uri_home: Vec::new(), // need to keep strings alive otherwise lilv memory goes boom
+            plugin_names: HashMap::new(),
             in_buf: [0.0; 2],
             out_buf: [0.0; 2],
         }
     }
 
-    pub unsafe fn add_plugin(&mut self, uri: &str) -> Result<(), String>{
+    pub unsafe fn add_plugin(&mut self, uri: &str, name: String) -> Result<(), String>{
         let mut uri_src = uri.to_owned();
         uri_src.push('\0'); // make sure it's null terminated
         self.uri_home.push(uri_src);
@@ -43,7 +46,7 @@ impl Lv2Host{
         let plugin = lilv_plugins_get_by_uri(self.lilv_plugins, uri);
         lilv_node_free(uri);
 
-        let (mut ports, n_audio_in, n_audio_out) = create_ports(self.world, plugin);
+        let (mut ports, port_names, n_audio_in, n_audio_out) = create_ports(self.world, plugin);
         if n_audio_in != 2 || n_audio_out != 2 {
             return Err(format!("TermDaw: plugin audio input and output ports must be 2. \n\t Audio input ports: {}, Audio output ports: {}", n_audio_in, n_audio_out));
         }
@@ -75,9 +78,31 @@ impl Lv2Host{
         self.plugins.push(Plugin{
             lilv_plugin: plugin,
             instance,
+            port_names,
+            ports,
         });
+        self.plugin_names.insert(name, self.plugins.len() - 1);
 
         Ok(())
+    }
+
+    fn set_port_value(plug: &mut Plugin, port: &str, value: f32) -> bool{
+            let port_index = plug.port_names.get(port);
+            if port_index.is_none() { return false; }
+            let port_index = port_index.unwrap();
+            let min = plug.ports[*port_index].min;
+            let max = plug.ports[*port_index].max;
+            plug.ports[*port_index].value = value.max(min).min(max);
+            true
+    }
+
+    pub fn set_value(&mut self, plugin: &str, port: &str, value: f32) -> bool{
+        if let Some(index) = self.plugin_names.get(plugin){
+            let plug = &mut self.plugins[*index];
+            Self::set_port_value(plug, port, value)
+        } else {
+            false
+        }
     }
 
     pub unsafe fn apply_plugin(&mut self, index: usize, input_frame: (f32, f32)) -> (f32, f32){
@@ -107,6 +132,8 @@ enum PortType{ Control, Audio, Other }
 struct Plugin{
     lilv_plugin: *const LilvPlugin,
     instance: *mut LilvInstance,
+    port_names: HashMap<String, usize>,
+    ports: Vec<Port>,
 }
 
 struct Port{
@@ -122,17 +149,17 @@ struct Port{
     name: String,
 }
 
-// ([ports], n_audio_in, n_audio_out)
-unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> (Vec<Port>, usize, usize){
+// ([ports], [(port_name, index)], n_audio_in, n_audio_out)
+unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> (Vec<Port>, HashMap<String, usize>, usize, usize){
     if world.is_null() { panic!("TermDaw: create_ports: world is null."); }
     if plugin.is_null() { panic!("TermDaw: create_ports: plugin is null."); }
 
     let mut ports = Vec::new();
+    let mut names = HashMap::new();
     let mut n_audio_in = 0;
     let mut n_audio_out = 0;
 
     let n_ports = lilv_plugin_get_num_ports(plugin);
-    println!("-------- Ports found: {}", n_ports);
 
     let mins = vec![0.0f32; n_ports as usize];
     let maxs = vec![0.0f32; n_ports as usize];
@@ -151,11 +178,14 @@ unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> 
         let min = mins[i as usize];
         let max = maxs[i as usize];
         let value = if def.is_nan() { 0.0 } else { def };
+
         let lilv_name = lilv_port_get_name(plugin, lport);
         let lilv_str = lilv_node_as_string(lilv_name);
         let c_str = CStr::from_ptr(lilv_str as *const i8);
         let name = c_str.to_str().expect("TermDaw: could not build port name string.").to_owned();
-        println!("{}: {} in [{}, {}]", name, value, mins[i as usize], maxs[i as usize]);
+
+        names.insert(name.clone(), i as usize);
+
         let optional = lilv_port_has_property(plugin, lport, lv2_connection_optional);
 
         let is_input = if lilv_port_is_a(plugin, lport, lv2_input_port) { true }
@@ -181,11 +211,11 @@ unsafe fn create_ports(world: *mut LilvWorld, plugin: *const LilvPluginImpl) -> 
         });
     }
 
-    lilv_node_free(lv2_input_port);
-    lilv_node_free(lv2_output_port);
-    lilv_node_free(lv2_audio_port);
-    lilv_node_free(lv2_control_port);
     lilv_node_free(lv2_connection_optional);
+    lilv_node_free(lv2_control_port);
+    lilv_node_free(lv2_audio_port);
+    lilv_node_free(lv2_output_port);
+    lilv_node_free(lv2_input_port);
 
-    (ports, n_audio_in, n_audio_out)
+    (ports, names, n_audio_in, n_audio_out)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ use crate::lv2::*;
 fn main() {
     unsafe{
         let mut host = Lv2Host::new();
-        host.add_plugin("http://calf.sourceforge.net/plugins/Compressor").expect("TermDaw: could not add plugin");
-        host.add_plugin("http://calf.sourceforge.net/plugins/Crusher").expect("TermDaw: could not add plugin");
+        host.add_plugin("http://calf.sourceforge.net/plugins/Compressor", "compressor".to_owned()).expect("TermDaw: could not add plugin");
+        host.add_plugin("http://calf.sourceforge.net/plugins/Crusher", "crusher".to_owned()).expect("TermDaw: could not add plugin");
+        let x = host.set_value("compressor", "Ratio", 2.0);
+        println!("{}", x);
+        // doesn't work if second+, port value locations change on vector resize?
+        // host.add_plugin("http://calf.sourceforge.net/plugins/Reverb", "reverb".to_owned()).expect("TermDaw: could not add plugin");
 
         let args: Vec<String> = std::env::args().collect();
         let file = &args[1];
@@ -27,7 +31,7 @@ fn main() {
             let r = s.unwrap() as f32 / i16::MAX.abs() as f32;
 
             let (l, r) = host.apply_plugin(0, (l,r));
-            let (l, r) = host.apply_plugin(1, (l,r));
+            //let (l, r) = host.apply_plugin(1, (l,r));
             writer.write_sample((l * i16::MAX.abs() as f32) as i16)
                 .expect("Error: could not write sample");
             writer.write_sample((r * i16::MAX.abs() as f32) as i16)
